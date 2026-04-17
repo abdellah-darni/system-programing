a4fa589bd5c63b36e6ea55e990ad86607b1ef91d
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-04-17 07:45:51 +0100

    test scripts

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+gcc -o file_info file_info.c
+gcc -o my_chmod my_chmod.c
+gcc -o my_chown my_chown.c
+gcc -o my_chgrp my_chgrp.c
+gcc -o my_unlink my_unlink.c
+
+touch test_target.txt
+
+./file_info test_target.txt
+
+./my_chmod test_target.txt u x
+./file_info test_target.txt
+
+sudo ./my_chgrp root test_target.txt
+./file_info test_target.txt
+
+sudo ./my_chown root test_target.txt
+./file_info test_target.txt
+
+sudo ./my_unlink test_target.txt
